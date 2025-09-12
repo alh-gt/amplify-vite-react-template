286aaf93c714649484fde6595620c3a4c6f69f9d
alha <imakoko111@gmail.com>
2025-09-12 15:49:45 +0900

    configureを1回だけ呼ぶように変更

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+// import outputs from "../amplify_outputs.json";
 import amplifyConfig from './amplify-config';
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 Amplify.configure(amplifyConfig)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
